37df017daa9866716f6a7ade7177fc21e700b5b1
Hoang Kiem <kiemhd@outlook.com>
2024-04-20 02:06:13 +0700

    chore: clean

--- functions.php
+++ functions.php
@@ -140,13 +140,9 @@ add_action( 'widgets_init', 'xdevlabs_widgets_init' );
  * Enqueue scripts and styles.
  */
 function xdevlabs_scripts() {
-	wp_enqueue_style( 'xdevlabs-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_style_add_data( 'xdevlabs-style', 'rtl', 'replace' );
-
+	wp_enqueue_style( 'xdevlabs-base', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_enqueue_style( 'font-awesome-5', '//cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css', null, '5.15.1' );
-
-	wp_enqueue_style( 'rhobii-styles', get_template_directory_uri() . '/styles.css', array(), wp_get_theme()->get( 'Version' ) );
-
+	wp_enqueue_style( 'xdevlabs', get_template_directory_uri() . '/assets/css/styles.css', array(), wp_get_theme()->get( 'Version' ) );
 	wp_enqueue_script( 'xdevlabs-navigation', get_template_directory_uri() . '/assets/js/main.js', array ( 'jquery' ), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
